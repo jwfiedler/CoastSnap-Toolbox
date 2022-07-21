5592214531c53ab6f030c64d25c67b79ed7d9ceb
Julia Fiedler <jfiedler@ucsd.edu>
2022-07-20 20:36:18 -0700

    more server updates

--- CSPsetPaths.m
+++ CSPsetPaths.m
@@ -1,3 +1,4 @@
 %Add relevant paths to Matlab
-github_path = '/Users/juliafiedler/Documents/CoastSnapDocs/CoastSnap/Code'; %Path where CIRN toolboxes (Coastsnap and Shoreline Mapping) are located
-addpath(genpath(github_path))
+% github_path = fileparts(pwd)
+% github_path = '/Users/juliafiedler/Documents/CoastSnapDocs/CoastSnap/Code'; %Path where CIRN toolboxes (Coastsnap and Shoreline Mapping) are located
+addpath(genpath(fullfile(base_path,'Code')))
