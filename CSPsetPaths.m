cf41bee176e1a7e0e1a917b8a3e333e1b8e92bc4
jwfiedler <jfiedler@ucsd.edu>
2023-09-06 13:56:19 -0700

    directory switcheroo
    when running on origin

--- CSPsetPaths.m
+++ CSPsetPaths.m
@@ -1,5 +1,5 @@
 %Add relevant paths to Matlab
 % github_path = fileparts(pwd)
-% github_path = '/Users/juliafiedler/Documents/CoastSnapDocs/CoastSnap/Code'; %Path where CIRN toolboxes (Coastsnap and Shoreline Mapping) are located
-addpath(genpath(fullfile(base_path,'Code')));
-% addpath(genpath(base_path))
+github_path = '/Users/jfiedler/Documents/Repositories'; %Path where CIRN toolboxes (Coastsnap and Shoreline Mapping) are located
+% addpath(genpath(fullfile(base_path,'Code')));
+addpath(genpath(github_path))
